1f336885e33dfbc61a78076449a8abea4531f26b
Hajimalung Baba <hajimalungbaba.786@gmail.com>
2026-01-15 21:06:57 +0530

    microfrontend setup is complete

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CinemaLayout } from './components/cinema-layout/cinema-layout';
 import { CinemaListComponent } from "./components/cinema-list/cinema-list";
 
@@ -8,10 +8,19 @@ import { CinemaListComponent } from "./components/cinema-list/cinema-list";
   imports: [CinemaListComponent],
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('cinema-listing');
 
-  getAvailableCinemas() {
+  movieId = signal('2');
 
+  ngOnInit(): void {
+      window.addEventListener('movieSelected',(event:Event)=>{
+        const customEvent = event as CustomEvent;
+        this.movieId.set(customEvent.detail.id);
+      });
+  }
+
+  getAvailableCinemas() {
+    window.removeEventListener('movieSelected',()=>{});
   }
 }
--- src/app/components/cinema-list/cinema-list.ts
+++ src/app/components/cinema-list/cinema-list.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, input, OnInit } from "@angular/core";
+import { Component, computed, effect, inject, input } from "@angular/core";
 import { cinemaListStore } from "../../store/cinema-list.store";
 import { MatExpansionModule } from '@angular/material/expansion';
 import { CinemaLayout } from "../cinema-layout/cinema-layout";
@@ -9,18 +9,19 @@ import { CinemaLayout } from "../cinema-layout/cinema-layout";
   styleUrl: './cinema-list.scss',
   imports: [MatExpansionModule, CinemaLayout]
 })
-export class CinemaListComponent implements OnInit
+export class CinemaListComponent
 {
   movieId = input<string>('1');
 
-  cinemasList = computed(()=>{
-    return this.cinemaStore.cinemas();
-  });
-
   cinemaStore = inject(cinemaListStore);
 
-  ngOnInit(): void {
+  cinemasList = computed(()=>{
+        return this.cinemaStore.cinemas();
+  });
+
+  // effect: call loadCinemasForMovie now and whenever `movieId` changes
+  movieLoader = effect(() => {
     this.cinemaStore.loadCinemasForMovie(this.movieId());
-  }
+  });
 
 }
